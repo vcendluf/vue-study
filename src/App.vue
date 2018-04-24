943045896eb3141d65941cc246f4c9977204cca3
darklao <fg716@foxmail.com>
2018-04-24 19:25:27 +0800

    initialize commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+html,body{
+  margin: 0;
+  height: 100%;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+  display: flex;
+  overflow: hidden;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,124 @@
+<template>
+  <el-container>
+      <el-header>
+        <el-menu
+  :default-active="activeIndex2"
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect"
+  background-color="#545c64"
+  text-color="#fff"
+  active-text-color="#ffd04b">
+  <el-menu-item index="1">处理中心</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">我的工作台</template>
+    <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+    <el-submenu index="2-4">
+      <template slot="title">选项4</template>
+      <el-menu-item index="2-4-1">选项1</el-menu-item>
+      <el-menu-item index="2-4-2">选项2</el-menu-item>
+      <el-menu-item index="2-4-3">选项3</el-menu-item>
+    </el-submenu>
+  </el-submenu>
+  <el-menu-item index="3" disabled>消息中心</el-menu-item>
+  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+</el-menu>
+      </el-header>
+  <el-container>
+  <el-aside width="200">
+      <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#63625ff6"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item>
+    </el-menu>
+  </el-aside>
+    <el-container>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</el-container>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      isCollapse: true,
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style>
+.el-header {
+  padding: 0;
+  background-color: #b3c0d1;
+  color: #333;
+  /* text-align: center; */
+    /* display: flex; */
+  /* overflow: hidden; */
+  line-height: 60px;
+}
+
+.el-header>.el-menu {
+  justify-content: center;
+    width: 100%;
+}
+
+.el-aside {
+  /* width: 200px; */
+  background-color: #63625ff6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+  /* display: flex; */
+  /* overflow: hidden; */
+}
+
+/* .el-menu{
+  background-color: #d3dce6;
+  width: 200px;
+} */
+
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  min-height: 600px;
+}
+</style>
